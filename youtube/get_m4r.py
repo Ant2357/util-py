6dd6eb78a8b0737139027aca2eacef8ea1b1b08e
Ant2357 <ryu4979@gmail.com>
2024-09-10 16:34:41 +0900

    fix: FileName入力欄を追加

--- youtube/get_m4r.py
+++ youtube/get_m4r.py
@@ -2,10 +2,11 @@ import yt_dlp
 from pydub import AudioSegment
 
 url = input("URL:")
+file_name = input("File Name:")
 
 ydl_opts = {
     'format': 'bestaudio',
-    'outtmpl':'output.%(ext)s',
+    'outtmpl': f'{file_name}.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -16,5 +17,5 @@ ydl_opts = {
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
 
-mp3_audio = AudioSegment.from_mp3('output.mp3')
-mp3_audio.export("output.m4r", format="mp4")
+mp3_audio = AudioSegment.from_mp3(f'{file_name}.mp3')
+mp3_audio.export(f"{file_name}.m4r", format="mp4")
