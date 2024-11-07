fdbd16c57a4bb6ed238e1d51ce90b895b88029cd
Ant2357 <ryu4979@gmail.com>
2024-11-08 01:48:05 +0900

    fix: 入力欄に数値しか入力できないように修正

--- auto/auto_click_gui/auto_clicker_gui.py
+++ auto/auto_click_gui/auto_clicker_gui.py
@@ -5,6 +5,7 @@ import time
 import keyboard
 from plyer import notification
 from PIL import Image, ImageTk
+import re
 
 import os
 import sys
@@ -23,47 +24,52 @@ class AutoClickerGUI:
         self.clicker = clicker
         self.running = True
 
-        root.title("SOVIET AUTO CLICKER")
-        root.protocol("WM_DELETE_WINDOW", self.quit_me)
-        root.geometry("300x300")
-        root.resizable(False, False)
-        root.iconphoto(True, PhotoImage(file=resource_path("assets/images/background.png")))
+        self.root.title("SOVIET AUTO CLICKER")
+        self.root.protocol("WM_DELETE_WINDOW", self.quit_me)
+        self.root.geometry("300x300")
+        self.root.resizable(False, False)
+        self.root.iconphoto(True, PhotoImage(file=resource_path("assets/images/background.png")))
 
         # 背景画像の設定
         image = Image.open(resource_path("assets/images/background.png"))
         resized_image = image.resize((300, 300), Image.Resampling.LANCZOS)
         self.background_image = ImageTk.PhotoImage(resized_image)
-        background_label = tk.Label(root, image=self.background_image)
+        background_label = tk.Label(self.root, image=self.background_image)
 
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         # タイトルラベル(ソ連スタイルのフォントと金と赤の色を使用)
-        title_label = tk.Label(root, text="КЛИКЕР", font=("Helvetica", 20, "bold"), fg="#FFD700", bg="#800000")
+        title_label = tk.Label(self.root, text="КЛИКЕР", font=("Helvetica", 20, "bold"), fg="#FFD700", bg="#800000")
         title_label.pack(pady=10)
 
         # 説明ラベル(後日変更する可能性有り)
-        label = tk.Label(root, text="クリック間隔を入力してください", font=("UD デジタル 教科書体 NK-B", 12, "bold"), fg="#FFD700", bg="#800000")
+        label = tk.Label(self.root, text="クリック間隔を入力してください", font=("UD デジタル 教科書体 NK-B", 12, "bold"), fg="#FFD700", bg="#800000")
         label.pack()
 
         # ホットキーショートカットの説明
-        keyLabel = tk.Label(root, text="F7: オートクリック開始\nF8: オートクリック停止", font=("UD デジタル 教科書体 NK-B", 10, "bold"), fg="#FFD700", bg="#800000")
+        keyLabel = tk.Label(self.root, text="F7: オートクリック開始\nF8: オートクリック停止", font=("UD デジタル 教科書体 NK-B", 10, "bold"), fg="#FFD700", bg="#800000")
         keyLabel.pack(pady=5)
 
+
         # クリック間隔の入力フィールド
-        self.entry = tk.Entry(root, font=("Helvetica", 12), justify='center', fg="#FFD700", bg="#333333")
+        vcmd = self.root.register(self.onValidateNumber)
+        self.entry = tk.Entry(self.root, validate="key", validatecommand=(vcmd, "%P"), font=("Helvetica", 12), justify='center', fg="#FFD700", bg="#333333")
         self.entry.insert(0, "0.5")
         self.entry.pack(pady=5)
 
         # ボタンデザイン(ソ連風、金と赤を使用)
-        start_button = tk.Button(root, text="Start", font=("Helvetica", 12, "bold"), fg="#800000", bg="#FFD700", command=self.start_clicking)
+        start_button = tk.Button(self.root, text="Start", font=("Helvetica", 12, "bold"), fg="#800000", bg="#FFD700", command=self.start_clicking)
         start_button.pack(pady=5)
 
-        stop_button = tk.Button(root, text="Stop", font=("Helvetica", 12, "bold"), fg="#800000", bg="#FFD700", command=self.stop_clicking)
+        stop_button = tk.Button(self.root, text="Stop", font=("Helvetica", 12, "bold"), fg="#800000", bg="#FFD700", command=self.stop_clicking)
         stop_button.pack(pady=5)
 
         # ホットキーリスナーをスレッドで実行する
         hotkey_thread = threading.Thread(target=self.hotkey_listener)
         hotkey_thread.start()
+
+    def onValidateNumber(self, str) -> bool:
+        return bool(re.fullmatch(r"^\d+(\.\d+)?$", str))
 
 
     def start_clicking(self) -> None:
